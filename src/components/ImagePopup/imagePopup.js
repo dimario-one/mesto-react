bbe89b74f386c5928770d173164ad5a70552d48b
Dimario <Dimario-one@yandex.ru>
2022-01-15 21:44:10 +0600

    fix title

--- src/components/ImagePopup/imagePopup.js
+++ src/components/ImagePopup/imagePopup.js
@@ -8,8 +8,8 @@ function ImagePopup(props) {
 <div className={`popup popup_type_image ${props.card ? 'popup_open' : " " }`}>
         <div className="popup__container">
             <button className="popup__close-button popup__close-image" type="button" onClick={props.onClose}></button>
-            <img className=" popup__pic" src={props.card.src} alt={props.card.name}/>
-            <h2 className="popup__title popup__title_white">{props.card.name}</h2>
+            <img className=" popup__pic" src={props.card.src} alt={props.card.title}/>
+            <h2 className="popup__title popup__title_white">{props.card.title}</h2>
         </div>
     </div>
   )
